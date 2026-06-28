ca6717e9bed09d97a1153c5251fecb6f95850785
tsukasa <qwerasdnoob@gmail.com>
2026-06-28 16:54:52 +0800

    Fixed an error

--- config.h
+++ config.h
@@ -1,10 +1,6 @@
 static void dwindle(Monitor *m);
-static void master(Monitor *m);
-static void monocle(Monitor *m);
 
 static const Layout layouts[] = {
     {"><>", NULL},
     {"[T]", dwindle},
-    {"[M]", master},
-    {"[]", monocle},
 };
